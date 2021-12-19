bbdd74c6bddb5b9d38c42cf5aaa33a297da8b2fc
i.brusenko <i.brusenko@redefine.family>
2021-12-19 19:11:55 +0700

    init

--- src/js/libs/gaPushEvents.js
+++ src/js/libs/gaPushEvents.js
@@ -1,13 +1,15 @@
 //Функция отправки событий в GA
 function gaPushEvent(action, category, label, nonInteraction) {
-  let event = nonInteraction ? 'eventWithNonInteraction' : 'event';
-
-  window['dataLayer']
-    ? window.dataLayer.push({
-        event: event,
-        eventCategory: category,
-        eventAction: action,
-        eventLabel: label,
-      })
-    : console.log(action, category, label, nonInteraction);
+  try {
+    !window.location.href.includes('localhost') && window['gtag']
+      ? gtag('event', action, {
+          event_category: category,
+          event_label: label,
+          non_interaction: nonInteraction,
+        })
+      : console.log(action, category, label, nonInteraction);
+  } catch (e) {
+    console.log(e);
+  }
 }
+//**OVER**
