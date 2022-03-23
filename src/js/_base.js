22780027e5aa2f00e45bf1175d9d3f4aadfbf7e5
i.brusenko <i.brusenko@redefine.family>
2022-03-23 13:36:58 +0700

    refactoring

--- src/js/_base.js
+++ src/js/_base.js
@@ -1,12 +1,12 @@
 // Это файлик для подключения библиотек-файлов
 
-//= include 'libs/zenscroll.js'
-//= include 'libs/aos.js'
+//= include '_libs/zenscroll.js'
+//= include '_libs/aos.js'
 
-//= include 'libs/deviceType.js'
-//= include 'libs/isTouchDevise.js'
+//= include '_helpers/deviceType.js'
+//= include '_helpers/isTouchDevise.js'
 
-//= include 'libs/getCoords.js'
-//= include 'libs/gaPushEvents.js'
+//= include '_helpers/getCoords.js'
+//= include '_helpers/gaPushEvents.js'
 
-//= include 'libs/castomEventListner.js'
+//= include '_helpers/castomEventListner.js'
